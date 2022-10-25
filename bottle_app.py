4b86a35ee8e3269ce949ee3ab54dc06761081c78
JaswanthGarikipati <Jgarikip@kent.edu>
2022-10-25 23:51:09 +0000

    First set of files

--- bottle_app.py
+++ bottle_app.py
@@ -4,7 +4,15 @@ from bottle import default_app, route
 
 @route('/')
 def hello_world():
-    return 'Hello from Bottle!'
+    return 'Hello from Jaswanth G!'
+
+@route('/bye')
+def bye_world():
+    return 'Bye from Jaswanth G!'
+
+@route('/list')
+def get_list():
+    return 'The shopping list goes here'
 
 application = default_app()
 
